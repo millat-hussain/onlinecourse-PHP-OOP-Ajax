6867bf584df6cd2e0670d33b053874a24fbeb21a
Millat <millathussain127166@gmail.com>
2021-03-05 10:33:48 +0600

    This Poject Onlinecourse

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; <?php echo date('d/m/y'); ?> Online Course Registration | By : <a href="http://www.facebook.com/developermillat" target="_blank">MD.Millat Hussain</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
